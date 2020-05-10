6fbd1a4e8cc13bbbaebd549cec847744462adae1
Kenn <sabatojnr01@gmail.com>
2020-05-10 18:36:38 +0300

    index.css index.html index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+
+var enoughSleep = true;
+if (enoughSleep) {
+  console.log("I'm ready to go!");
+} else {
+  console.log("I need a nap.");
+}
+var temperature = 30;
+undefined
+if (temperature < 20) {
+    console.log("Don't forget a jacket!");
+} else {
+    console.log("Keep cool today!");
+}
